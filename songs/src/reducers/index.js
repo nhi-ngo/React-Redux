bd6ab37529935434335851d3da77a9b5882ad202
nhi-ngo <nhingo.y@gmail.com>
2020-07-08 21:44:48 -0400

    building reducers

---
+++ songs/src/reducers/index.js
@@ -0,0 +1,17 @@
+// static list of songs that is unchanged so expecting no argument in the function
+const songsReducer = () => {
+  return [
+    { title: 'No Scrubs', duration: '4:05' },
+    { title: 'Macarena', duration: '2:30' },
+    { title: 'All Star', duration: '3:15' },
+    { title: 'I Want It That Way', duration: '1:45' },
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === 'SONG_SELECTED') {
+    return action.payload;
+  }
+
+  return selectSong;
+};
