c01094fc0154e362deda4bb4476ef3b86668db2a
nhi-ngo <nhingo.y@gmail.com>
2020-07-21 14:09:54 -0400

    component scaffolding

--- streams-react-router/client/src/components/App.js
+++ streams-react-router/client/src/components/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-          App
-    </div>
-  );
-}
+const App = () => {
+  return <div>App</div>;
+};
 
 export default App;
