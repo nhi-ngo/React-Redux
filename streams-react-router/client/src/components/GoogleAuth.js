126f88f6730283f1e0e8667e0bdbba084c07d1ea
nhi-ngo <nhingo.y@gmail.com>
2020-07-22 21:43:45 -0400

    display Sign In and Sign Out button

--- streams-react-router/client/src/components/GoogleAuth.js
+++ streams-react-router/client/src/components/GoogleAuth.js
@@ -14,17 +14,40 @@ class GoogleAuth extends Component {
         .then(() => {
           this.auth = window.gapi.auth2.getAuthInstance();
           this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+          this.auth.isSignedIn.listen(this.onAuthChange);
         });
     });
   }
 
+  // auth status changes without refreshing the page
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
+
+  onSignIn = () => {
+
+  }
+
+  onSignOut = () => {
+    
+  }
+
   renderAuthButton() {
     if (this.state.isSignedIn === null) {
-      return <div>I don't know if we are signed in</div>
+      return null;
     } else if (this.state.isSignedIn) {
-      return <div>I am signed in!</div>
+      return (
+        <button className="ui red google button">
+          <i className="google icon"></i>
+          Sign Out
+        </button>);
     } else {
-      return <div>I am not signed in</div>
+      return (
+        <button className="ui red google button">
+          <i className="google icon"></i>
+            Sign In
+        </button>
+      )
     }
   }
 
