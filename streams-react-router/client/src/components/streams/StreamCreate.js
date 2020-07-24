a7f3c6d78c6b40b99d79877cdf6d7fe789b45d88
nhi-ngo <nhingo.y@gmail.com>
2020-07-24 15:04:45 -0400

    handle form submission

--- streams-react-router/client/src/components/streams/StreamCreate.js
+++ streams-react-router/client/src/components/streams/StreamCreate.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
-};
+// eslint-disable-next-line react/prefer-stateless-function
+class StreamCreate extends Component {
+  renderInput({ input, label }) {
+    return (
+      <div className="field">
+        <label>{label}</label>
+        <input {...input} type="text" />
+      </div>
+    );
+  }
 
-export default StreamCreate;
+  onSubmit(formValues) {
+    console.log(formValues);
+  }
+
+  render() {
+    // console.log(this.props);
+    return (
+      <form className="ui form" onSubmit={this.props.handleSubmit(this.onSubmit)}>
+        <Field name="title" component={this.renderInput} label="Enter Title" />
+        <Field name="description" component={this.renderInput} label="Enter Description" />
+        <button className="ui button primary">Submit</button>
+      </form>
+    );
+  }
+}
+
+export default reduxForm({ form: 'streamCreate' })(StreamCreate);
