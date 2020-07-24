04ff9778fc9bddc267e5d8d2d49ee58cd821c708
nhi-ngo <nhingo.y@gmail.com>
2020-07-24 16:40:31 -0400

    show form error on touch

--- streams-react-router/client/src/components/streams/StreamCreate.js
+++ streams-react-router/client/src/components/streams/StreamCreate.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
-import { Field, reduxForm } from 'redux-form';
+import { Field, reduxForm, clearSubmitErrors } from 'redux-form';
 
-// eslint-disable-next-line react/prefer-stateless-function
 class StreamCreate extends Component {
-  renderInput({ input, label }) {
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  }
+
+  renderInput = ({ input, label, meta }) => {
     return (
       <div className="field">
         <label>{label}</label>
-        <input {...input} type="text" />
+        <input {...input} type="text" autoComplete="off" />
+        {this.renderError(meta)}
       </div>
     );
   }
@@ -19,7 +29,7 @@ class StreamCreate extends Component {
   render() {
     // console.log(this.props);
     return (
-      <form className="ui form" onSubmit={this.props.handleSubmit(this.onSubmit)}>
+      <form className="ui form error" onSubmit={this.props.handleSubmit(this.onSubmit)}>
         <Field name="title" component={this.renderInput} label="Enter Title" />
         <Field name="description" component={this.renderInput} label="Enter Description" />
         <button className="ui button primary">Submit</button>
@@ -28,4 +38,18 @@ class StreamCreate extends Component {
   }
 }
 
-export default reduxForm({ form: 'streamCreate' })(StreamCreate);
+const validate = (formValues) => {
+  const errors = {};
+
+  if (!formValues.title) {
+    errors.title = 'You must enter a title';
+  }
+
+  if (!formValues.description) {
+    errors.description = 'You must enter a description';
+  }
+
+  return errors;
+};
+
+export default reduxForm({ form: 'streamCreate', validate })(StreamCreate);
