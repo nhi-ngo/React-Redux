42d4114ba58daeed7e572bdbf023e0044434e30a
nhi-ngo <nhingo.y@gmail.com>
2020-08-03 16:53:24 -0400

    add useRef() and manual event listener

--- widgets-hook/src/components/Dropdown.js
+++ widgets-hook/src/components/Dropdown.js
@@ -1,7 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
   const [open, setOpen] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+    document.body.addEventListener('click', (event) => {
+      // check if elements being clicked on are inside of the form div then return immediately. Otherwise go ahead and call setOpen(false)
+      if (ref.current.contains(event.target)) {
+        return;
+      }
+
+      setOpen(false); // close the dropdown
+    });
+  }, []);
 
   const renderedOptions = options.map((option) => {
     // Filtering the options list. Null means do not render anything.
@@ -15,7 +27,7 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
   });
 
   return (
-    <div className="ui form">
+    <div ref={ref} className="ui form">
       <div className="field">
         <label className="label">Select Color</label>
         <div onClick={() => setOpen(!open)} className={`ui selection dropdown ${open ? 'visible active' : ''}`}>
