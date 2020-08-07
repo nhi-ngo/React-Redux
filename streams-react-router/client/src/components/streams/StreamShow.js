e52cfa27f3e4a622a3aed3fcd4ed9babc06fd63d
nhi-ngo <nhingo.y@gmail.com>
2020-08-07 13:32:39 -0400

    create StreamShow component

--- streams-react-router/client/src/components/streams/StreamShow.js
+++ streams-react-router/client/src/components/streams/StreamShow.js
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
-const StreamShow = () => {
-  return <div>StreamShow</div>;
+class StreamShow extends Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  render() {
+    if (!this.props.stream) {
+      return (
+        <div className="ui segment">
+          <div className="ui active dimmer">
+            <div className="ui text loader">Loading</div>
+          </div>
+          <p />
+        </div>
+      );
+    }
+
+    const { title, description } = this.props.stream;
+
+    return (
+      <div>
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default StreamShow;
+export default connect(mapStateToProps, { fetchStream })(StreamShow);
