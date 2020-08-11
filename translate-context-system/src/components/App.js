ea7f1f15d38e53f9ebc614d71c1b16ead0167a9a
nhi-ngo <nhingo.y@gmail.com>
2020-08-11 17:35:04 -0400

    pulling from multiple contexts

--- translate-context-system/src/components/App.js
+++ translate-context-system/src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import UserCreate from './UserCreate';
 import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
 
 class App extends Component {
   state = { language: 'english' };
@@ -17,9 +18,11 @@ class App extends Component {
           <i onClick={() => this.onLanguageChange('english')} className="flag us" />
           <i onClick={() => this.onLanguageChange('vietnamese')} className="flag vn" />
         </div>
-        <LanguageContext.Provider value={this.state.language}>
-          <UserCreate />
-        </LanguageContext.Provider>
+        <ColorContext.Provider value="red">
+          <LanguageContext.Provider value={this.state.language}>
+            <UserCreate />
+          </LanguageContext.Provider>
+        </ColorContext.Provider>
       </div>
     );
   }
--- translate-context-system/src/components/Button.js
+++ translate-context-system/src/components/Button.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
 import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
 
 class Button extends Component {
   renderSubmit(value) {
     return value === 'english' ? 'Submit' : 'Gửi đi';
   }
 
-  render() {
+  renderButton(color) {
     return (
-      <button className="ui button primary">
+      <button className={`ui button ${color}`} type="submit">
         <LanguageContext.Consumer>{(value) => this.renderSubmit(value)}</LanguageContext.Consumer>
       </button>
     );
+  }
+
+  render() {
+    return <ColorContext.Consumer>{(color) => this.renderButton(color)}</ColorContext.Consumer>;
   }
 }
 
