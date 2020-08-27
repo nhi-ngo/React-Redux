807bafb8a86e1075a4d0d6d5bd26be261ba03536
nhi-ngo <nhingo.y@gmail.com>
2020-08-27 16:23:52 -0400

    redux-promise middleware

---
+++ weather/src/reducers/weatherReducer.js
@@ -0,0 +1,10 @@
+import { FETCH_WEATHER } from '../actions';
+
+export default (state = null, action) => {
+  switch (action.type) {
+    case FETCH_WEATHER:
+      return action.payload;
+    default:
+      return state;
+  }
+};
