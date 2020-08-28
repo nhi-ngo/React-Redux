60c0cf374bdf35b9aee75036a605603c5e819f78
nhi-ngo <nhingo.y@gmail.com>
2020-08-28 14:23:36 -0400

    make reusable Chart component

--- weather/src/containers/WeatherList.js
+++ weather/src/containers/WeatherList.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Chart from '../components/Chart';
 
 class WeatherList extends Component {
   renderWeather() {
     return this.props.weather.map(cityData => {
       const { id, name } = cityData.city;
-      const { temp, pressure, humidity } = cityData.list[0].main;
+      const temps = cityData.list.map(weather => weather.main.temp);
+      const pressures = cityData.list.map(weather => weather.main.pressure);
+      const humidities = cityData.list.map(weather => weather.main.humidity);
 
       return (
         <tr key={id}>
-          <th scope="row">{name}</th>
-          <td>{temp}</td>
-          <td>{pressure}</td>
-          <td>{humidity}</td>
+          <td>{name}</td>
+          <td>
+            <Chart data={temps} color="orange" units="K" />
+          </td>
+          <td>
+            <Chart data={pressures} color="green" units="hPa" />
+          </td>
+          <td>
+            <Chart data={humidities} color="blue" units="%" />
+          </td>
         </tr>
       );
     });
@@ -24,9 +33,9 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temperature (K) </th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
         <tbody>{this.renderWeather()}</tbody>
